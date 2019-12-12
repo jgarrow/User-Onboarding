44c5bde97a2cdd3c7d04e60b1001ca94c983641f
Janessa Garrow <janessa.garrow@gmail.com>
2019-12-11 23:31:03 -0700

    Add styling to form. Add validation for checkbox

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
@@ -9,26 +9,28 @@ import axios from "axios";
 // Terms of Service (checkbox)
 // A Submit button to send our form data to the server
 
-const UserForm = ({ values, errors, touched, status }) => {
+const UserForm = ({ values, errors, touched, status, isSubmitting }) => {
+    const [users, setUsers] = useState([]);
+
     console.log("errors:", errors);
     console.log("touched:", touched);
 
-    // useEffect(() => {
-    //     console.log("status has changed:", status);
-    //     status && setAnimals(animals => [...animals, status]);
-    // }, [status]);
+    useEffect(() => {
+        console.log("status has changed:", status);
+        status && setUsers(users => [...users, status]);
+    }, [status]);
 
     return (
         <div>
             <Form>
                 <Field type="text" name="name" placeholder="Name here" />
                 {touched.name && errors.name && (
-                    <p className="errors">{errors.name}</p>
+                    <p className="errors">{`*${errors.name}`}</p>
                 )}
 
                 <Field type="email" name="email" placeholder="Email here" />
                 {touched.email && errors.email && (
-                    <p className="errors">{errors.email}</p>
+                    <p className="errors">{`* ${errors.email}`}</p>
                 )}
 
                 <Field
@@ -37,21 +39,36 @@ const UserForm = ({ values, errors, touched, status }) => {
                     placeholder="Password here"
                 />
                 {touched.password && errors.password && (
-                    <p className="errors">{errors.password}</p>
+                    <p className="errors">{`* ${errors.password}`}</p>
                 )}
 
                 <label className="checkbox-container">
-                    Agree to Terms of Service
                     <Field
                         type="checkbox"
-                        name="terms"
-                        checked={values.terms}
+                        name="acceptedTerms"
+                        checked={values.acceptedTerms}
                     />
+                    Agree to Terms of Service
                     <span className="checkmark" />
                 </label>
+                {touched.acceptedTerms && errors.acceptedTerms && (
+                    <p className="errors">
+                        * You must accept the Terms of Service
+                    </p>
+                )}
 
-                <button type="submit">Submit</button>
+                <button type="submit" disabled={isSubmitting}>
+                    Submit
+                </button>
             </Form>
+            {users.map(user => {
+                return (
+                    <ul key={user.id}>
+                        <li>Name: {user.name}</li>
+                        <li>Email: {user.email}</li>
+                    </ul>
+                );
+            })}
         </div>
     );
 };
@@ -62,13 +79,18 @@ const FormikForm = withFormik({
             name: props.name || "",
             email: props.email || "",
             password: props.password || "",
-            terms: props.terms || false
+            acceptedTerms: props.acceptedTerms || false
         };
     },
     validationSchema: Yup.object().shape({
         name: Yup.string().required(),
         email: Yup.string().required(),
-        password: Yup.string().required()
+        password: Yup.string()
+            .min(8, "Too Short!")
+            .required(),
+        acceptedTerms: Yup.boolean()
+            .required()
+            .oneOf([true], "You must accept the terms and conditions.")
     }),
     handleSubmit(values, { setStatus, resetForm }) {
         console.log("submitting", values);
